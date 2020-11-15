2f9e49d693280ccca53f3203595ba3d4986daea8
vikipretium <vighneshpasupathy@gmail.com>
2020-11-15 15:41:43 +0530

    10 excercies completed

---
+++ lp3hw/10.What_was_that?.py
@@ -0,0 +1,20 @@
+# what was that?
+
+tabby_cat = "\t I'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll dot a list:
+\t* Cat food
+\t* Fishes
+\t* Catnip\n\t* Grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
+print(" a \\ b \\ c ")
+print("a \ b \ c")
+
---
+++ lp3hw/11.asking_questions.py
@@ -0,0 +1,11 @@
+# asking questions
+
+print("How old are you?", end=' ')
+age = input()
+print("How tall are you?", end=' ')
+height = input()
+print("How much do you weigh?", end=' ')
+weight = input()
+
+print(f"So, you're {age} year old, {height} tall and {weight} heavy.")
+
